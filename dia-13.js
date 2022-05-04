4e5d9bb0474d973b271fc072d6818d58dd288992
Martin Milone <77274994+tinchomilo@users.noreply.github.com>
2022-05-04 18:05:37 -0300

    dia13

---
+++ dia-13.js
@@ -0,0 +1,46 @@
+/*
+¡Hay demasiados regalos 🎁! Y envolverlos es una locura...
+
+Vamos a crear una función que pasándole un array de regalos, nos devuelva otro array pero donde todos los regalos han sido envueltos con asteriscos tanto por arriba como por los lados.
+
+Sólo tienes que tener en cuenta unas cosillas ✌️:
+
+Si el array está vacío, devuelve un array vacío
+Los regalos son emojis 🎁... por lo que tenlo en cuenta a la hora de contar su longitud...
+Por suerte, cada posición del array siempre tiene la misma longitud...
+wrapGifts(["📷", "⚽️"])
+/* Resultado:
+[ '****',
+  '*📷*',
+  '*⚽️*',
+  '****'
+]
+*/
+
+wrapGifts(["🏈🎸", "🎮🧸"])
+/* Resultado:
+[ '******',
+  '*🏈🎸*',
+  '*🎮🧸*',
+  '******'
+]
+*/
+
+wrapGifts(["📷"])
+/* Resultado:
+[ '****',
+  '*📷*',
+  '****'
+]
+*/
+
+export default function wrapGifts(gifts) {
+    let newGifts = gifts.map( elem => '*'+ elem +'*')
+      let acum = ''
+      for (let i = 0; i < gifts[0].length + 2; i++) {
+          acum += '*'
+      }
+      newGifts.push(acum)
+      newGifts.unshift(acum)
+      return newGifts
+  }
